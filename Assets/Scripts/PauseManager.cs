509f4fc4bccc66874b0fff95c32cb51b7676b3f9
pdh9747-design <pdh9747@gmail.com>
2026-08-13 17:34:03 +0900

    GameUIUX

--- Assets/Scripts/PauseManager.cs
+++ Assets/Scripts/PauseManager.cs
@@ -1,17 +1,33 @@
 using UnityEngine;
 using UnityEngine.InputSystem;
+using UnityEngine.EventSystems;
 
 public class PauseManager : MonoBehaviour
 {
     public GameObject pausePanel;
+    public GameObject resumeButton;
 
     void Update()
     {
         if (Keyboard.current.escapeKey.wasPressedThisFrame)
         {
             bool isPaused = pausePanel.activeSelf;
+
             pausePanel.SetActive(!isPaused);
+
+            if (!isPaused)
+            {
+                StartCoroutine(SelectResumeButton());
+            }
         }
+    }
+
+    private System.Collections.IEnumerator SelectResumeButton()
+    {
+        yield return null;
+
+        EventSystem.current.SetSelectedGameObject(null);
+        EventSystem.current.SetSelectedGameObject(resumeButton);
     }
 
     public void ResumeGame()
